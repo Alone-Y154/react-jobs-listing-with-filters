3b9cb806428bc7b036594ef6318d483af820d1b3
Alone-Y154 <yashwanthkris154@gmail.com>
2023-10-11 00:41:51 +0530

    Added the Filters UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import data from './data.json';
 import JobsList from './components/JobsList'
@@ -6,15 +6,14 @@ import JobsList from './components/JobsList'
 const Filter = (props) => {
 
   const filterName = props.filterName;
-  const index = props.index;
-  console.log("inside filter")
+  const key = props.key;
   return (
     <div  className=' flex h-8 cursor-pointer rounded-[4px] bg-[#5CA5A5] bg-opacity-10 items-center'>
       <div className='pl-2 '>
         <p className='text-[#5CA5A5] opacity-100 bg-opacity-10 text-base font-bold tracking-[-0.123px] '>{filterName}</p>
       </div>
-      <div  onClick={props.filterRemove} className='hover:bg-[#2B3939] bg-[#5CA5A5] w-8 h-8 flex items-center justify-center ml-2 rounded-r-[4px]'>
-        <img src="./images/icon-remove.svg" alt="remove" value={index} />
+      <div  onClick={()=> props.filterRemove(key)} className='hover:bg-[#2B3939] bg-[#5CA5A5] w-8 h-8 flex items-center justify-center ml-2 rounded-r-[4px]'>
+        <img src="./images/icon-remove.svg" alt="remove"  />
       </div>
 
     </div>
@@ -32,7 +31,7 @@ const FilterBox = (props) => {
 
           {filterList.map((filterName, index) => {
             return (
-              <Filter filterRemove = {props.filterRemove} key={index} index={index} filterName={filterName} />
+              <Filter filterRemove = {() => props.filterRemove(index)} key={index} filterName={filterName} />
             )
           })}
         </div>
@@ -54,25 +53,33 @@ function App(props) {
 
   const jobData = data;
   const [filterList, setFilterList] = useState([]);
-  // console.log(jobData)
+
+  const filterArray = [];
 
   const tagArray = (e) => {
     const tag = e.target.innerText;
     setFilterList(current =>
       [...new Set([...current, tag])] );
-    console.log(tag)
-
+    console.log("inside tag array",filterList) 
   }
+  console.log("outside tag array",filterList) 
+  
 
   const filterClear = () => {
     setFilterList([]);
   }
 
-  const filterRemove = (e) => {
-    console.log("index" ,e)
-  }
 
-  console.log(filterList)
+  const filterRemove = (e) => {
+    if (e >= 0 && e < filterList.length) {
+      const updatedFilterList = [...filterList]; // Create a copy of the array
+      updatedFilterList.splice(e, 1); // Remove the element
+      setFilterList(updatedFilterList); // Update the state
+    }
+  }
+  
+  console.log("outside filterRemove",filterList) 
+
 
   return (
     <div className={`jobs-container bg-[#5CA5A5] bg-opacity-10 ${!filterList.length && "h-[100vh]"}`}>
